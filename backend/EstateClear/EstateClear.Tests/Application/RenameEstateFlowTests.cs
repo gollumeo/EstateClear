c32334f0561e0f3824e546576ab76c65250d3dd3
Pierre "Golluméo" Mauriello <hire-me@pierre-mauriello.be>
2025-11-27 17:28:08 +0100

    feat(tests): add validation test for renaming closed estates in RenameEstateFlow

--- backend/EstateClear/EstateClear.Tests/Application/RenameEstateFlowTests.cs
+++ backend/EstateClear/EstateClear.Tests/Application/RenameEstateFlowTests.cs
@@ -1,9 +1,6 @@
-using System;
-using System.Threading.Tasks;
 using EstateClear.Application;
 using EstateClear.Domain.Estates.Entities;
 using EstateClear.Domain.Estates.ValueObjects;
-using Xunit;
 
 namespace EstateClear.Tests.Application;
 
@@ -27,6 +24,30 @@ public class RenameEstateFlowTests
         Assert.NotNull(result);
         Assert.Single(estates.SavedEstates);
         Assert.Equal("Estate Beta", estates.SavedEstates[0].DisplayName().Value());
+        Assert.Empty(estates.RenamedEstates);
+    }
+
+    [Fact]
+    public async Task RenameEstateFlowShouldRejectRenamingAClosedEstate()
+    {
+        var estateId = EstateId.From(Guid.NewGuid());
+        var executorId = ExecutorId.From(Guid.NewGuid());
+        var aggregate = Estate.Create(estateId, executorId, EstateName.From("Estate Alpha"));
+        aggregate
+            .GetType()
+            .GetField("<Status>k__BackingField", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic)
+            ?.SetValue(aggregate, EstateStatus.Closed);
+        var input = new RenameEstate(estateId, "Estate Beta");
+        var estates = new EstatesFake
+        {
+            LoadedEstate = aggregate
+        };
+        var flow = new RenameEstateFlow(estates);
+
+        var action = () => flow.Execute(input);
+
+        await Assert.ThrowsAnyAsync<Exception>(action);
+        Assert.Empty(estates.SavedEstates);
         Assert.Empty(estates.RenamedEstates);
     }
 }
