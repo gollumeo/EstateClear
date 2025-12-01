ae46e943e75aa1b80249d86bafec5bb38b66145b
Pierre "Golluméo" Mauriello <hire-me@pierre-mauriello.be>
2025-12-01 16:53:37 +0100

    feat(persistence): add EF Core support with repositories and tests for users and estates
    - Introduced `EstateClearDbContext` with `UserRecord` and `UserRecordConfiguration`.
    - Added `UsersRepositoryEf` and `EstatesRepositoryEf` for managing users and estates.
    - Implemented integration tests for `UsersRepositoryEf` and `EstatesRepositoryEf` with SQLite in-memory DB.
    - Updated dependencies to include `Microsoft.EntityFrameworkCore.Sqlite`.

---
+++ backend/EstateClear/EstateClear.Persistence/EF/EstateClearDbContext.cs
@@ -0,0 +1,15 @@
+using EstateClear.Persistence.EF.Configurations;
+using EstateClear.Persistence.EF.Records;
+using Microsoft.EntityFrameworkCore;
+
+namespace EstateClear.Persistence.EF;
+
+public sealed class EstateClearDbContext(DbContextOptions<EstateClearDbContext> options) : DbContext(options)
+{
+    public DbSet<UserRecord> Users => Set<UserRecord>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new UserRecordConfiguration());
+    }
+}
